fb1fd261b07bfa42f0183134b3c66f9e21ba3e97
Ichmagkase <78343787+Ichmagkase@users.noreply.github.com>
2023-08-19 21:08:07 -0400

    Made updates to main, organized files,

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Redux\Documents\GitHub\Pynd\venv
+set VIRTUAL_ENV=C:\Users\micro\Documents\GitHub\Pynd\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
