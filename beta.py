9037d2366d2b0156e8cfc192d8302ea044a21fa9
Ichmagkase <78343787+Ichmagkase@users.noreply.github.com>
2023-08-26 22:47:03 -0400

    Fixed my own error

--- beta.py
+++ beta.py
@@ -46,7 +46,7 @@ class MyWindow(QMainWindow):
         self.setGeometry(200, 200, 500, 750)
         self.setWindowTitle("Pynd")
 
-        self.instanceDatabase = 'kitchen'
+        self.instanceDatabase = 'kitchen.db'
         self.inst = dbRetriever(self.instanceDatabase)
         self.itemIndex = 0
 
