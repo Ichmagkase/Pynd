785de34bd786c280ec75a95fbfe3fa53ae518cbc
Ichmagkase <78343787+Ichmagkase@users.noreply.github.com>
2023-09-08 15:38:12 -0400

    bug fixes and error handling, updated README

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
 # Pynd
-A dnd interface built entirely with pyqt5 and sqlite3                 
+A dnd interface built in Python 3.10 using PyQt5 and Sqlite3 libraries        
 
-This is my first personal project that i will use for my friends and i to play dnd easier
+!!!HOW TO RUN!!!
+
+To run beta.py (main), run it in the command line with
+''''
+python beta.py itemsList.db
+''''
+
+
+Description:
+------------------------------------------------------------------------------------------------------------------------------
+This is my first personal project with the intent to introduce the convinience of modern technology to the age old game: Dungeons and Dragons. While the most popular forms of campaigns are conviniently organized in large DND guides sold in book stores, many home brewed campaigns must rely on methods that make organizing and searching for items and their properties extremely difficult on a larger scale. 
+
+The purpose of this personal project is to learn how to apply Python within a practical application along with learning the process of app platform development and organization and analysis of data along the way.
+-------------------------------------------------------------------------------------------------------------------------------
+
+Guide:
+-------------------------------------------------------------------------------------------------------------------------------
+Databases (directory): holds copies of the database and csv currently not in use by main (beta.py)
+Item_descriptions (directory): contains .txt files to be read by main for item descriptions
+venv (local python virtual environemnt)
+
+beta.py:
+    The main running file of the project, includes PyQt5 and Sqlite3. 
+    
+    MyWindow() makes and manages the current GUI and calls dbRetriever() using the current active database.
+
+    dbRetriever() reads the current item database into a list to be indexed for use in MyWindow()
+
+csvtodb.py:
+    Basic .csv to .db converter made for reading .csv tables exported from Excell or Sheets structured for the purposes of this app into a .db file usable by Sqlite3. Takes 2 command line arguments: The name of the new sqlite3 table, and the CSV file
+    to be converted into that table.
+
+itemsList.db: Current active database, a copy of ItemsList.db in /Databases
+
+schema.txt: Used to organize properties of items into tables TO BE CREATED AND IMPLIMENTED into sqlite3.
+
+a TODO list is also included to provide transparancy on what is currently in progress or in the process of brainstorming.
--- beta.py
+++ beta.py
@@ -3,20 +3,25 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 import sqlite3
 
-
+# called by MyWindow.__init__() with one argument
 class dbRetriever:
+    #initialize sqlite3 with the database
     def __init__(self, database):
         self.databaseName = database
         self.conn = sqlite3.connect(database)
         print(f"successfully opened {database}\n--------------------")
-        self.cursor = self.conn.cursor()
+        self.cursor = self.conn.cursor() 
 
+
+    #index names from table into a list
     def indexItems(self):
         self.itemNames = self.cursor.execute(f"SELECT name FROM {self.databaseName[:-3]}") # ignore .db suffix 
         self.itemList = []
         for i in self.itemNames:
             self.itemList.append(i)
         return self.itemList
+
+    #open the description of an item if the item has a description .txt associated with it. Otherwise return None
     def openDescription(self, item):
         self.desc = self.cursor.execute(f"SELECT desc FROM {self.databaseName[:-3]} WHERE name = ?", (item,))
         self.desc2 = []
@@ -26,14 +31,16 @@ class dbRetriever:
         if self.desc2[0][0] == "None":
             return "None"
 
-        with open(f'item_descriptions/{self.desc2[0][0]}', 'r') as f: #! can be shortened to return f.read() ?
+        with open(f'item_descriptions/{self.desc2[0][0]}', 'r') as f:
             contents = f.read()
             return contents
 
+    # return the 'type' property of the selected item
     def openType(self, item):
         self.type = self.cursor.execute(f"SELECT type FROM {self.databaseName[:-3]} WHERE name = ?", (item,))
         return self.type
 
+    # return the 'dmg' property of the selected item
     def openDmg(self, item):
         self.dmg = self.cursor.execute(f"SELECT dmg FROM {self.databaseName[:-3]} WHERE name = ?" , (item,))
         return self.dmg
@@ -42,21 +49,28 @@ class dbRetriever:
 class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__() 
-        #xpos, ypos, width, height
-        self.setGeometry(200, 200, 500, 750)
+        #xpos, ypos, width, height, of the window
+        self.setGeometry(200, 200, 550, 750)
         self.setWindowTitle("Pynd")
 
-        self.instanceDatabase = 'itemsList.db'
+        #BETA TESTING ONLY: Initialize the app with a .db file within the command line arguments at runtime
+        try:
+            self.instanceDatabase = sys.argv[1]
+        except:
+            print("Missing argv[1], please include a .db file to be opened")
+            exit()
         self.inst = dbRetriever(self.instanceDatabase)
         self.itemIndex = 0
 
         self.initUI()
 
     def initUI(self):
+        #create a label with default text 'item'
         self.label1 = QtWidgets.QLabel(self)
         self.label1.setText("item")
         self.label1.move(250, 100)
 
+        #create a label with default text 'desc'
         self.label2 = QtWidgets.QLabel(self)
         self.label2.setText("desc") #set label to the currently selected property
         self.label2.move(150, 500)
@@ -64,25 +78,28 @@ class MyWindow(QMainWindow):
         #button to cycle item
         self.b1 = QtWidgets.QPushButton(self)
         self.b1.setText("Next Item")
-        self.b1.clicked.connect(self.cycleItem) #when the button is clicked, change label1 to the currently selected item
+        self.b1.clicked.connect(self.cycleItem) #when the button is clicked, call cycleItem()
         self.b1.move(20, 100)
 
         #buttons to select properties
         self.b2 = QtWidgets.QPushButton(self)
         self.b2.setText("Type")
-        self.b2.clicked.connect(self.displayType) #when the button is clicked, change label1 to the currently selected item
+        self.b2.clicked.connect(self.displayType) #when the button is clicked, call displayType()
         self.b2.move(100, 250)
 
         self.b3 = QtWidgets.QPushButton(self)
         self.b3.setText("Damage")
-        self.b3.clicked.connect(self.displayDamage) #when the button is clicked, change label1 to the currently selected item
+        self.b3.clicked.connect(self.displayDamage) #when the button is clicked, call displayDamage()
         self.b3.move(200, 250)
 
         self.b4 = QtWidgets.QPushButton(self)
         self.b4.setText("Description")
-        self.b4.clicked.connect(self.displayDescription) #when the button is clicked, change label1 to the currently selected item
+        self.b4.clicked.connect(self.displayDescription) #when the button is clicked, call displayDescription()
         self.b4.move(300, 250)
 
+        
+
+    #cycle through itemList list and display it to label1
     def cycleItem(self):
         self.itemList = self.inst.indexItems()
         if self.itemIndex == (len(self.itemList))-1:
@@ -95,13 +112,18 @@ class MyWindow(QMainWindow):
 
         self.update()
 
+    #Update the labels to fit the new text
     def update(self):
         self.label1.adjustSize()
         self.label2.adjustSize()
 
-    #Buttons for displaying properties
+    #call openType() from dbRetriever() and display the return value to label2
     def displayType(self):
-        self.type = self.inst.openType(self.selectItem)
+        try:
+            self.type = self.inst.openType(self.selectItem)
+        except:
+            print("Please select 'Next Item' before selecting the item type, damage, or description. This will be fixed in the future.")
+            exit()
         displaytype = []
 
         for i in self.type:
@@ -111,9 +133,13 @@ class MyWindow(QMainWindow):
 
         self.update()
         
+    #call openDmg() from dbRetriever and display the return value to label2
     def displayDamage(self):
-
-        self.damage = self.inst.openDmg(self.selectItem)
+        try:
+            self.damage = self.inst.openDmg(self.selectItem)
+        except:
+            print("Please select 'Next Item' before selecting the item type, damage, or description. This will be fixed in the future.")
+            exit()
         displaydamage = []
 
         for i in self.damage:
@@ -122,9 +148,13 @@ class MyWindow(QMainWindow):
         self.label2.setText(str(displaydamage[0][0]))
         self.update()
 
+    #call openDescription() from dbRetriever() and display the return value to label2
     def displayDescription(self):
-
-        self.desc = self.inst.openDescription(self.selectItem)
+        try:
+            self.desc = self.inst.openDescription(self.selectItem)
+        except:
+            print("Please select 'Next Item' before selecting the item type, damage, or description. This will be fixed in the future.")
+            exit()
         displaydesc = []
         for i in self.desc:
             displaydesc.append(i)
@@ -132,10 +162,11 @@ class MyWindow(QMainWindow):
         self.label2.setText(self.desc)
         self.update()
 
-
+#main function call that starts and exits the app
 def window():
-    app = QApplication(sys.argv)
-    win = MyWindow()
+    app = QApplication(sys.argv) 
+    win = MyWindow() 
+
     win.show()
     sys.exit(app.exec_())
 
